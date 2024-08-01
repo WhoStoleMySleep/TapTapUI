e71dc453d3aa7218314ffb5fb27282000bf405a4
Konstantin Naumenko <98680950+WhoStoleMySleep@users.noreply.github.com>
2024-08-01 21:33:56 +0700

    test(Input): Improve "test" - add snapshots

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,22 +1,80 @@
 import React from "react";
 import { cleanup, render } from "@testing-library/react";
-import { Title } from "./Title";
+import { Input } from "./Input";
 
 afterEach(cleanup);
 
-describe("Component: Title", () => {
+describe("Component: Input", () => {
   const children =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  it("Occupancy", () => {
-    const { getByText }: any = render(<Title>{children}</Title>);
+  it("Value Occupancy", () => {
+    const { getByDisplayValue }: any = render(<Input>{children}</Input>);
 
-    expect(getByText(children)).toBeTruthy();
+    expect(getByDisplayValue(children)).toBeTruthy();
   });
 
-  it("Snapshot", () => {
-    const title: any = render(<Title>{children}</Title>);
+  describe("Snapshots", () => {
+    describe("Snapshots: type", () => {
+      it("Snapshot: Text type", () => {
+        const inputText: any = render(<Input type={"text"}>{children}</Input>);
 
-    expect(title).toMatchSnapshot();
+        expect(inputText).toMatchSnapshot();
+      });
+    });
+
+    describe("Snapshots: className", () => {
+      it("Snapshot: short className", () => {
+        const inputShortClassName: any = render(
+          <Input className={"n"}>{children}</Input>,
+        );
+
+        expect(inputShortClassName).toMatchSnapshot();
+      });
+
+      it("Snapshot: long className", () => {
+        const inputLongClassName: any = render(
+          <Input className={children}>{children}</Input>,
+        );
+
+        expect(inputLongClassName).toMatchSnapshot();
+      });
+    });
+
+    describe("Snapshots: UI", () => {
+      it("Snapshot: Standard", () => {
+        const inputStandard: any = render(
+          <Input UI={"standard"}>{children}</Input>,
+        );
+
+        expect(inputStandard).toMatchSnapshot();
+      });
+
+      it("Snapshot: Primary", () => {
+        const inputPrimary: any = render(
+          <Input UI={"primary"}>{children}</Input>,
+        );
+
+        expect(inputPrimary).toMatchSnapshot();
+      });
+    });
+
+    describe("Snapshots: noDarkMode", () => {
+      it("Snapshot: noDarkMode - false", () => {
+        const inputStandard: any = render(
+          <Input noDarkMode={false}>{children}</Input>,
+        );
+
+        expect(inputStandard).toMatchSnapshot();
+      });
+
+      it("Snapshot: noDarkMode - true", () => {
+        const inputPrimary: any = render(
+          <Input noDarkMode={true}>{children}</Input>,
+        );
+
+        expect(inputPrimary).toMatchSnapshot();
+      });
+    });
   });
 });
