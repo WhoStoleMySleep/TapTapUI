d609273e76c6702b2f42b4186f1f20bb52a99622
Konstantin Naumenko <98680950+WhoStoleMySleep@users.noreply.github.com>
2024-08-01 20:21:52 +0700

    test(Title): Improve "test", add Snapshots

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -14,9 +14,185 @@ describe("Component: Title", () => {
     expect(getByText(children)).toBeTruthy();
   });
 
-  it("Snapshot", () => {
-    const title: any = render(<Title>{children}</Title>);
+  describe("Snapshots", () => {
+    describe("UI Snapshots", () => {
+      it("Snapshot: Standard", () => {
+        const titleStandard: any = render(
+          <Title UI={"standard"}>{children}</Title>,
+        );
 
-    expect(title).toMatchSnapshot();
+        expect(titleStandard).toMatchSnapshot();
+      });
+
+      it("Snapshot: Primary", () => {
+        const titlePrimary: any = render(
+          <Title UI={"primary"}>{children}</Title>,
+        );
+
+        expect(titlePrimary).toMatchSnapshot();
+      });
+    });
+
+    describe("Level Snapshots", () => {
+      describe("Snapshots: Level - Standard UI", () => {
+        it("Snapshot: h1", () => {
+          const titleH1: any = render(
+            <Title UI={"standard"} Level={"h1"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH1).toMatchSnapshot();
+        });
+
+        it("Snapshot: h2", () => {
+          const titleH2: any = render(
+            <Title UI={"standard"} Level={"h2"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH2).toMatchSnapshot();
+        });
+
+        it("Snapshot: h3", () => {
+          const titleH3: any = render(
+            <Title UI={"standard"} Level={"h3"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH3).toMatchSnapshot();
+        });
+
+        it("Snapshot: h4", () => {
+          const titleH4: any = render(
+            <Title UI={"standard"} Level={"h4"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH4).toMatchSnapshot();
+        });
+
+        it("Snapshot: h5", () => {
+          const titleH5: any = render(
+            <Title UI={"standard"} Level={"h5"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH5).toMatchSnapshot();
+        });
+
+        it("Snapshot: h6", () => {
+          const titleH6: any = render(
+            <Title UI={"standard"} Level={"h6"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH6).toMatchSnapshot();
+        });
+      });
+
+      describe("Snapshots: Level - Primary UI", () => {
+        it("Snapshot: h1", () => {
+          const titleH1: any = render(
+            <Title UI={"primary"} Level={"h1"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH1).toMatchSnapshot();
+        });
+
+        it("Snapshot: h2", () => {
+          const titleH2: any = render(
+            <Title UI={"primary"} Level={"h2"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH2).toMatchSnapshot();
+        });
+
+        it("Snapshot: h3", () => {
+          const titleH3: any = render(
+            <Title UI={"primary"} Level={"h3"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH3).toMatchSnapshot();
+        });
+
+        it("Snapshot: h4", () => {
+          const titleH4: any = render(
+            <Title UI={"primary"} Level={"h4"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH4).toMatchSnapshot();
+        });
+
+        it("Snapshot: h5", () => {
+          const titleH5: any = render(
+            <Title UI={"primary"} Level={"h5"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH5).toMatchSnapshot();
+        });
+
+        it("Snapshot: h6", () => {
+          const titleH6: any = render(
+            <Title UI={"primary"} Level={"h6"}>
+              {children}
+            </Title>,
+          );
+
+          expect(titleH6).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe("Snapshots: className", () => {
+      it("Snapshot: short className", () => {
+        const titleClassName: any = render(
+          <Title className={"t"}>{children}</Title>,
+        );
+
+        expect(titleClassName).toMatchSnapshot();
+      });
+
+      it("Snapshot: long className", () => {
+        const titleClassName: any = render(
+          <Title className={children}>{children}</Title>,
+        );
+
+        expect(titleClassName).toMatchSnapshot();
+      });
+    });
+
+    describe("Snapshots: noDarkMode", () => {
+      it("Snapshot: noDarkMode - false", () => {
+        const titleNoDarkMode: any = render(
+          <Title noDarkMode={false}>{children}</Title>,
+        );
+
+        expect(titleNoDarkMode).toMatchSnapshot();
+      });
+
+      it("Snapshot: noDarkMode - true", () => {
+        const titleNoDarkMode: any = render(
+          <Title noDarkMode={true}>{children}</Title>,
+        );
+
+        expect(titleNoDarkMode).toMatchSnapshot();
+      });
+    });
   });
 });
