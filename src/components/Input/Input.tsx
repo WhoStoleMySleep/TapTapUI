bd6dd41eabeb2b9285a7ec61883fc95a5e383e4f
Konstantin Naumenko <98680950+WhoStoleMySleep@users.noreply.github.com>
2024-08-01 21:33:17 +0700

    feat(Input): Add displaying "children" of props as "value" of input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Input.module.scss";
 import { InputProps } from "./Input.types";
 
@@ -12,16 +12,25 @@ export const Input = ({
   ...attributes
 }: InputProps) => {
   const acceptableUI = ["standard", "primary"];
+  const [value, setValue] = useState(children);
 
   return (
     <>
       {!acceptableUI.find((ui) => ui === UI) || UI === "standard" ? (
-        <input className={className} type={type} {...attributes} />
+        <input
+          className={className}
+          type={type}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          {...attributes}
+        />
       ) : (
         <input
           className={styles.input + " " + className}
           type={type}
           data-dark-mode={!noDarkMode}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
           {...attributes}
         />
       )}
