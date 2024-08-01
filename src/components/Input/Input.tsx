38c0bbe5ed5bd9452e76113c84ed469f7208e216
Konstantin Naumenko <98680950+WhoStoleMySleep@users.noreply.github.com>
2024-08-01 21:17:59 +0700

    refactor(Input): Improve "Input.tsx" component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,17 +3,28 @@ import styles from "./Input.module.scss";
 import { InputProps } from "./Input.types";
 
 /** Интерактивная карточка */
-export const Input: ({
+export const Input = ({
   children,
-  type,
-  className,
-  UI,
+  type = "text",
+  className = "",
+  UI = "standard",
+  noDarkMode = false,
   ...attributes
-}: InputProps) => React.JSX.Element = ({ children, type, className, UI, ...attributes }) => (
-  <>
-    {UI === "standard" ?
-      <input className={className} type={type} {...attributes} /> :
-      <input className={styles.input + " " + className} type={type} {...attributes} />
-    }
-  </>
-);
+}: InputProps) => {
+  const acceptableUI = ["standard", "primary"];
+
+  return (
+    <>
+      {!acceptableUI.find((ui) => ui === UI) || UI === "standard" ? (
+        <input className={className} type={type} {...attributes} />
+      ) : (
+        <input
+          className={styles.input + " " + className}
+          type={type}
+          data-dark-mode={!noDarkMode}
+          {...attributes}
+        />
+      )}
+    </>
+  );
+};
